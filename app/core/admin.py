677e7fd1171d4cb01fee3d17df61ef3768295b50
AlbertCardona <albert.cardona.03@gmail.com>
2023-05-30 19:39:33 -0600

    Errors resolved

--- app/core/admin.py
+++ app/core/admin.py
@@ -4,7 +4,7 @@ Django admin customization
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-import models
+from core import models
 
 
 class UserAdmin(BaseUserAdmin):
