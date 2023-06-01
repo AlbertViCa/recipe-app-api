f36147e33a190f582939a79fdf3ec802e5049cd1
AlbertViCa <albert0-v.c@hotmail.com>
2023-06-01 16:06:22 -0600

    test

--- app/core/tests/test_user_api.py
+++ app/core/tests/test_user_api.py
@@ -99,12 +99,6 @@ class PublicUserApiTests(TestCase):
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
-    def test_retrieve_user_unauthorized(self):
-        """Test authentication is required for users."""
-        res = self.client.get(reverse('user:me'))
-
-        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
-
     def test_create_user_with_no_name(self):
         """Test creating a user with no name."""
         payload = {
